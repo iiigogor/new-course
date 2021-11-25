5e72b066f4ee5d07d41a914ff7080524862b5585
unknown <iiigogor@gmail.com>
2021-11-25 14:41:39 +0300

    initial

---
+++ main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        int balance = 2000000000;
+        int translation = 500000000;
+        int totalSum = balance + translation;
+        System.out.println(totalSum);
+    }
+}
